bcdf8a4a868c0e4e0590151baf16ec5a0ce0be4b
Martin Journois <martin.journois@fr.michelin-solutions.com>
2016-09-27 09:39:45 +0200

    Create the GET byColor service

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import tornado.web
 import tornado.wsgi
 import wsgiref.simple_server
 import os
+import json
 
-print(__path__)#data_file = os
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, world\n")
+
+
+class ByColor(tornado.web.RequestHandler):
+    def get(self, color):
+        color = color.lower()
+        if color not in ['green', 'yellow', 'orange', 'red']:
+            raise tornado.web.HTTPError(400, reason='Unknown color {}'.format(color))
+        data = json.load(open('./data.json'))
+        self.write("{} : {}\n".format(color, data.get(color, 0)))
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/byColor/(.*?)", ByColor),
     ])
 
 if __name__ == "__main__":
